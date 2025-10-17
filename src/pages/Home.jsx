99a98da21df2c03c899ca10c698a783775b0e3e3
sebastian <manijustworkhere@gmail.com>
2025-10-17 10:17:50 -0400

    im done at least for now this project was to get familiar or at least more comfortable with certain react functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { data, useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (
+            <div
+              className="user"
+              onClick={() => navigate(`${user.id}`)}
+              key={user.id}
+            >
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>{user.name}</h3>
+                  <p>
+                    <b>Email:</b> {user.email}
+                  </p>
+                  <p>
+                    <b>Phone:</b> {user.phone}
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    {user.website}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Posts = () => {
+  let navigate = useNavigate();
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
   //const [loading, setLoading] = useState(true);
@@ -29,9 +30,7 @@ const Posts = () => {
   return (
     <>
       <div className="post__search">
-        <Link to="/">
-          <button>← Black</button>
-        </Link>
+        <button onClick={() => navigate("/")}>← Black</button>
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
           <input
